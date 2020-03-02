a599af553d52c600b147f219f4492edd4c12d469
Marius <marius.tw@hotmail.no>
2020-03-02 01:12:09 +0100

    Double quotes fixed

--- server.py
+++ server.py
@@ -86,10 +86,10 @@ def get_users():
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        s.connect(('10.255.255.255', 1))
+        s.connect(("10.255.255.255", 1))
         ip = s.getsockname()[0]
     except:
-        ip = '127.0.0.1'
+        ip = "127.0.0.1"
     finally:
         s.close()
     return ip
